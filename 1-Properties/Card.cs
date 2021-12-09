dfcf94b9035abe0b4ac2250f5201f9d2c4f551d0
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-09 21:18:00 +0100

    implemented Equals methods and GetHashCode

--- 1-Properties/Card.cs
+++ 1-Properties/Card.cs
@@ -5,7 +5,7 @@ namespace Properties
     /// <summary>
     /// The class models a card.
     /// </summary>
-    public class Card
+    public class Card : IEquatable<Card>
     {
         private readonly string seed;
         private readonly string name;
@@ -33,32 +33,39 @@ namespace Properties
         }
 
         // TODO improve
-        public string GetSeed()
-        {
-            return this.seed;
-        }
+        public string Seed => seed;
 
         // TODO improve
-        public string GetName()
-        {
-            return this.name;
-        }
+        public string Name => name;
 
         // TODO improve
-        public int GetOrdinal()
-        {
-            return this.ordinal;
-        }
+        public int Ordinal => ordinal;
 
         /// <inheritdoc cref="object.ToString"/>
         public override string ToString()
         {
             // TODO understand string interpolation
-            return $"{this.GetType().Name}(Name={this.GetName()}, Seed={this.GetSeed()}, Ordinal={this.GetOrdinal()})";
+            return $"{this.GetType().Name}(Name={this.Name}, Seed={this.Seed}, Ordinal={this.Ordinal})";
         }
 
         // TODO generate Equals(object obj)
+        public override bool Equals(object obj)
+        {
+            return (obj.GetType() == GetType()) && (obj is null) && Equals(obj as Card);
+        }
+
+        public bool Equals(Card other)
+        {
+            return other != null &&
+                   Seed == other.Seed &&
+                   Name == other.Name &&
+                   Ordinal == other.Ordinal;
+        }
 
         // TODO generate GetHashCode()
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Seed, Name, Ordinal);
+        }
     }
 }
